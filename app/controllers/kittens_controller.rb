1810ab7d2281640b379b8755dad9c7df7594442d
richie <rsaldiva@gmail.com>
2015-11-02 21:49:07 -0500

    add rest routes, views, and controller actions to kitten model

---
+++ app/controllers/kittens_controller.rb
@@ -0,0 +1,57 @@
+class KittensController < ApplicationController
+  def index
+    @kittens = Kitten.all
+  end
+  
+  def show 
+    @kitten = Kitten.find(params[:id])
+  end
+  
+  
+  def new 
+    @kitten = Kitten.new
+  end
+  
+  def edit 
+   @kitten = Kitten.find(params[:id])
+  end
+  
+  def create
+  @kitten = Kitten.new(kitten_params)
+    if @kitten.save
+       redirect_to kittens_path
+    else
+      render 'new'
+    end
+  end
+  
+  
+  def update 
+  @kitten = Kitten.find(params[:id])
+   respond_to do |format|
+      if @kitten.update(kitten_params)
+        format.html { redirect_to @kitten, notice: 'Kitten was successfully updated.' }
+        format.json { render :show, status: :ok, location: @kitten }
+      else
+        format.html { render :edit }
+        format.json { render json: @kitten.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+  
+   def destroy
+   @kitten = Kitten.find(params[:id])
+    @kitten.destroy
+    respond_to do |format|
+      format.html { redirect_to kittens_url, notice: 'Kitten was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+  
+  private 
+   def kitten_params
+      params.require(:kitten).permit(:name, :age, :cuteness, :softness) 
+   end
+end
+
+
